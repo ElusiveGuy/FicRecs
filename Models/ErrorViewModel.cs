6e29f29a0523e57fdc92ec48f55d5b854d2e090c
Elu <elu@elusiveguy.com>
2018-10-15 04:20:00 +0000

    Initial mvc webapp

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FicRecs.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
