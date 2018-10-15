a1790f85503b944c6c1ce0a4995e4cd897712a2d
Elu <elu@elusiveguy.com>
2018-10-15 18:05:12 +1100

    Test spreadsheet

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using FicRecs_ExcelImporter.Models;
+using OfficeOpenXml;
 
 namespace FicRecs_ExcelImporter
 {
@@ -6,7 +10,29 @@ namespace FicRecs_ExcelImporter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length < 1)
+            {
+                Console.WriteLine($"USAGE: {Environment.GetCommandLineArgs()[0]} importfile.xlsx");
+                return;
+            }
+
+            var file = new FileInfo(args[0]);
+            if (!file.Exists)
+            {
+                Console.WriteLine($"{args[0]} does not exist");
+                return;
+            }
+
+            
+            using (var p = new ExcelPackage(file))
+            {
+                using (var context = new FicrecDbContext())
+                {
+                    var detailsheet = p.Workbook.Worksheets["Fic info"];
+                    var cols = new Dictionary<string, int>();
+                    Console.WriteLine(detailsheet.Cells[1, 1]);
+                }
+            }
         }
     }
 }
