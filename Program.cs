3f329830b264d0ab342fb89ab2bd2f3991ec2347
Elu <elu@elusiveguy.com>
2018-10-16 12:46:10 +1100

    Fix row/col

--- Program.cs
+++ Program.cs
@@ -29,36 +29,36 @@ namespace FicRecs_ExcelImporter
                 {
                     var detailsheet = p.Workbook.Worksheets["Fic info"];
                     var colnames = new Dictionary<string, int>();
-                    for (int col = 1; detailsheet.Cells[col, 1].Value != null; col++)
+                    for (int col = 1; detailsheet.Cells[1, col].Value != null; col++)
                     {
-                        colnames[detailsheet.Cells[col, 1].GetValue<string>()] = col;
+                        colnames[detailsheet.Cells[1, col].GetValue<string>()] = col;
                     }
 
-                    T GetCol<T>(string colname, int row)
+                    T GetCol<T>(int row, string colname)
                     {
-                        return detailsheet.Cells[colnames[colname], row].GetValue<T>();
+                        return detailsheet.Cells[row, colnames[colname]].GetValue<T>();
                     }
 
                     var rowidmap = new Dictionary<int, int>();
                     
-                    for (int row = 2; !String.IsNullOrWhiteSpace(GetCol<string>("ID", row)); row++)
+                    for (int row = 2; !String.IsNullOrWhiteSpace(GetCol<string>(row, "ID")); row++)
                     {
                         var story = new StoryDetails
                         {
-                            StoryId = GetCol<int>("ID", row),
-                            Title = GetCol<string>("Title", row),
-                            Author = GetCol<string>("Author", row),
-                            Summary = GetCol<string>("Summary", row),
-                            Characters = GetCol<string>("Characters", row),
-                            Chapters = GetCol<short>("Chapters", row),
-                            Words = GetCol<int>("Words", row),
-                            Reviews = GetCol<int>("Reviews", row),
-                            Favs = GetCol<int>("Favs", row),
-                            Follows = GetCol<int>("Follows", row),
-                            Published = GetCol<DateTime>("Published", row),
-                            Url = GetCol<string>("Url", row)
+                            StoryId = GetCol<int>(row, "ID"),
+                            Title = GetCol<string>(row, "Title"),
+                            Author = GetCol<string>(row, "Author"),
+                            Summary = GetCol<string>(row, "Summary"),
+                            Characters = GetCol<string>(row, "Characters"),
+                            Chapters = GetCol<short>(row, "Chapters"),
+                            Words = GetCol<int>(row, "Words"),
+                            Reviews = GetCol<int>(row, "Reviews"),
+                            Favs = GetCol<int>(row, "Favs"),
+                            Follows = GetCol<int>(row, "Follows"),
+                            Published = GetCol<DateTime>(row, "Published"),
+                            Url = GetCol<string>(row, "Url")
                         };
-                        var complete = GetCol<string>("Complete", row);
+                        var complete = GetCol<string>(row, "Complete");
                         if (complete == "Complete")
                             story.Complete = true;
                         else if (complete == "Incomplete")
@@ -68,13 +68,13 @@ namespace FicRecs_ExcelImporter
 
                         Console.WriteLine(story.StoryId);
                         
-                        rowidmap[GetCol<int>("Row", row)] = GetCol<int>("ID", row);
+                        rowidmap[GetCol<int>(row, "Row")] = GetCol<int>(row, "ID");
                     }
 
                     var matrixsheet = p.Workbook.Worksheets["Adjacency matrix"];
-                    for (int row = 1, col = 1; matrixsheet.Cells[col, row].Value != null; row++, col = 1)
+                    for (int row = 1, col = 1; matrixsheet.Cells[row, col].Value != null; row++, col = 1)
                     {
-                        for (; matrixsheet.Cells[col, row].Value != null; col++)
+                        for (; matrixsheet.Cells[row, col].Value != null; col++)
                         {
                             var matrix = new StoryMatrix()
                             {
