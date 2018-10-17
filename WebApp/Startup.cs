fd55a89322d59c57a125e08ca5dda98343b190d5
Elu <elu@elusiveguy.com>
2018-10-17 17:22:45 +1100

    Add DB service

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FicRecs.DatabaseLib;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -33,6 +34,8 @@ namespace FicRecs.WebApp
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<FicRecsDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
