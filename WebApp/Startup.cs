460f6bf0e7fcac3dcb8a2560f4ab37dc86150421
Elu <elu@elusiveguy.com>
2018-10-17 17:24:07 +1100

    Add DB service

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -35,7 +36,10 @@ namespace FicRecs.WebApp
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            services.AddDbContext<FicRecsDbContext>();
+            services.AddDbContext<FicRecsDbContext>(options =>
+            {
+                options.UseNpgsql("Host=/var/run/postgresql;Database=db");
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
