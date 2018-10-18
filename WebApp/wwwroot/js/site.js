db08298e83f7c4110c14a44725c16828d3f11607
Elu <elu@elusiveguy.com>
2018-10-18 16:42:35 +1100

    Pagination test

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -2,4 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-console.log('test');
+document.querySelectorAll('.pagination-link').forEach(l => {
+    if (!location.search)
+        l.href = '?page=' + l.dataset['page'];
+    else
+        l.href = location.search.replace(/page=\d+/, 'page=' + l.dataset['page']);
+});
